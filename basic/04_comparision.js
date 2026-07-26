72694eb6628a21287a35e4e6f1eac376886412e6
Poojita16 <poojitachaturvedi1606@gmail.com>
2026-07-27 00:19:58 +0530

    comparision of datatypes

---
+++ basic/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// == (checks only value )
+//=== ( checks both value and datatype,)
+
+console.log("2" === 2);
