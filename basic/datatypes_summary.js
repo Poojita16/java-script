b640920fb50b76497cbb311dc567e920d25e9240
Poojita16 <poojitachaturvedi1606@gmail.com>
2026-07-27 00:56:33 +0530

    datatype summary

---
+++ basic/datatypes_summary.js
@@ -0,0 +1,35 @@
+// Primitive data types in JS
+
+// 7 types: String, Number, Boolean, Null, Undefined,Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 1223455678899668n
+
+
+
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Poojita",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
