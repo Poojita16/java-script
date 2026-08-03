6b5a1cd0b9a34a0368f81fb1515e5d3b416f7387
Poojita16 <poojitachaturvedi1606@gmail.com>
2026-08-04 01:00:00 +0530

    object_1

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+//const tinderUsern = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Bali"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "pooju@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "poojita",
+            lastname: "chaturvedi"
+        }
+    }
+
+}
+
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+
+const users = [
+    { 
+    },
+    {
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
